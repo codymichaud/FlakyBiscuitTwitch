0682aceccee8c8cade76510c69acf17c09655d7c
cody <cody@ascendcohealth.com>
2023-01-04 23:04:17 -0600

    fixed sanity studio error

--- mangotechstore/sanity.config.js
+++ mangotechstore/sanity.config.js
@@ -3,15 +3,15 @@ import {deskTool} from 'sanity/desk';
 import {visionTool} from '@sanity/vision';
 import {schemaTypes} from './schemas';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = 'fm96vt2j';
+const dataset = 'production';
 
 export default defineConfig({
   basePath: '/studio',
   name: 'Spit_Roast_GanG_Content_Studio',
   title: 'Spit Roast GanG Content Studio',
-  projectId,
-  dataset,
+  projectId: projectId,
+  dataset: dataset,
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
