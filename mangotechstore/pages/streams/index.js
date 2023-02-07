cce491d4ca0963e31ca48f33c1aadf3bdc6d9e4d
cody <cody@ascendcohealth.com>
2023-02-06 20:28:31 -0600

    updated streams page

--- mangotechstore/pages/streams/index.js
+++ mangotechstore/pages/streams/index.js
@@ -2,44 +2,38 @@ import React from "react";
 import { createClient } from 'next-sanity';
 import { Link, Grid, Col, Text, Card } from "@nextui-org/react";
 
-export default function Streams({ firstStream }) {
-    console.log('whats this', firstStream[0])
+export default function Streams({ games }) {
+    console.log('whats this', games)
 
-    const renderStreamBoxes = () => {
-
-        return (
-                <Card>
-                    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-                    <Col>
-                        <Text size={25} weight="bold" transform="uppercase" color="#ffffffAA">
-                        {firstStream[0].firstGameTitle}
-                        </Text>
-                    </Col>
-                    </Card.Header>
-                    <Card.Image
-                    src='/Images/f1-2022.jpg'
-                    objectFit="cover"
-                    width="100%"
-                    height={340}
-                    alt="Card image background"
-                    />
-                </Card>
-        );
+    const renderStreamBoxes = (game) => {
+        
+            console.log('whats this game', game)
+            return (
+                <Grid.Container lg={12} gap={2}>
+                    <Grid xs={12} sm={12}>
+                        <Card>
+                            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                            <Col>
+                                <Text size={25} weight="bold" transform="uppercase" color="#ffffffAA">
+                                {game.firstGameTitle}
+                                </Text>
+                            </Col>
+                            </Card.Header>
+                            <Card.Image
+                            src={game.firstGameImgLink}
+                            objectFit="cover"
+                            width="100%"
+                            height={340}
+                            alt="Card image background"
+                            />
+                        </Card>
+                    </Grid>
+                </Grid.Container>
+            );
     }
         return (
-            <div>
-               <Grid.Container gap={2} justify="center">
-                        <Grid xs={12} sm={4}>
-                           {renderStreamBoxes()}
-                        </Grid>
-                        <Grid xs={12} sm={4}>
-                            {renderStreamBoxes()}
-                        </Grid>
-                        <Grid xs={12} sm={4}>
-                            {renderStreamBoxes()}
-                        </Grid>
-
-                </Grid.Container>
+            <div className="grid grid-cols-3">
+               {games.map(game => renderStreamBoxes(game))}
             </div>
         )
 }
@@ -52,11 +46,11 @@ const client = createClient({
   });
   
   export async function getStaticProps() {
-    const firstStream = await client.fetch(`*[_type == "firstStreamBox"]`);
+    const games = await client.fetch(`*[_type == "streamGames"]`);
   
     return {
       props: {
-        firstStream,
+        games,
       }
     }
   }
